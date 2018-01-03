4205a710f5463153bd4ee7bca6757f2beecfa135
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2018-01-03 16:04:32 -0600

    fix boolean typo

--- db/migrate/20180103215038_create_cells.rb
+++ db/migrate/20180103215038_create_cells.rb
@@ -4,7 +4,7 @@ class CreateCells < ActiveRecord::Migration[5.1]
       t.belongs_to :strip, index: true, foreign_key: true
       t.string :image_url, null: false
       t.integer :sort_order, null: false
-      t.bool :framed, null: false
+      t.boolean :framed, null: false
       t.timestamps
     end
   end
