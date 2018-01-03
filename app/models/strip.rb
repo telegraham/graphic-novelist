768e50f534189884cf71c5994a2471cdf5d694da
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2018-01-03 16:29:32 -0600

    set strip slug and published_at before save

--- app/models/strip.rb
+++ app/models/strip.rb
@@ -1,9 +1,16 @@
 class Strip < ApplicationRecord
 
   before_save :set_slug_unless_already_there
+  before_save :set_published_at_if_needed
 
   private
     def set_slug_unless_already_there
       self.slug = self.title.parameterize
+    end
+
+    def set_published_at_if_needed
+      if self.published?
+        self.published_at ||= Time.now.utc
+      end
     end
 end
