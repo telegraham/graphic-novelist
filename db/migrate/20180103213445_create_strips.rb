2960b4e53cf6612bbb01aadad94bbeaec1f7d539
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2018-01-03 16:16:24 -0600

    stub to slug in strip

--- db/migrate/20180103213445_create_strips.rb
+++ db/migrate/20180103213445_create_strips.rb
@@ -2,7 +2,7 @@ class CreateStrips < ActiveRecord::Migration[5.1]
   def change
     create_table :strips do |t|
       t.string :title, null: false
-      t.string :stub, null: false
+      t.string :slug, null: false
       t.boolean :published, null: false
       t.datetime :published_at
       t.timestamps
