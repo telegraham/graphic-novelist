b26e54d281e86f5b935208d355e8f6224f907549
Graham Troyer-Joy <grahamtroyerjoy@gmail.com>
2018-01-03 22:44:50 -0600

    seed cells

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,12 @@
 
 
 10.times do 
-  Strip.create(title: Faker::HarryPotter.quote, 
+  @strip = Strip.create(title: Faker::HarryPotter.quote, 
               published: Faker::Boolean.boolean(0.7))
+
+  8.times do |idx|
+    @strip.cells << Cell.new(image_url: Faker::Placeholdit.image("480x320", 'gif', 'ff8866'),
+                             sort_order: idx, 
+                             framed: Faker::Boolean.boolean(0.5))
+  end
 end
